bb58e26db7821077a68eba66f26e91ee92dc46c0
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2024-07-09 21:01:05 -0300

    reponsividade moblie 5

--- src/pages/Movie-Styles.js
+++ src/pages/Movie-Styles.js
@@ -26,13 +26,13 @@ export const ContainerContent = styled.section`
  border: 1px solid yellow;
 
  @media screen and (max-width: 768px){
-  width: 78%;
+  width: 80%;
  }
  @media screen and (max-width: 576px){
-  width: 78%;
+  width: 80%;
  }
  @media screen and (max-width: 490px){
-  width: 88%;
+  width: 90%;
  }
 `
 
